97313e0dc506e728cd961bd758377a893bce6f0c
toaonly42 <toaonly42@gmail.com>
2024-09-10 14:58:50 +0900

    lint: eslint, prettier 적용으로 인한 파일 변경

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
+<script setup lang="ts">
+import { useCounterStore } from './store'
+
+const { count } = $(useCounterStore())
+</script>
+
 <template>
   <div class="text-center py-4">
-    <router-link
-      active-class="text-teal-600 font-bold"
-      to="/"
-    >
-      Home
-    </router-link>|
-    <router-link
-      active-class="text-teal-600 font-bold"
-      to="/about"
-    >
-      About
-    </router-link>
+    <router-link active-class="activated" to="/"> Home </router-link>|
+    <router-link active-class="activated" to="/about"> About </router-link>
   </div>
-  <div class="text-lg py-4 text-center border-t border-b border-gray-400 dark:border-stone-400">
+  <div
+    class="text-lg py-4 text-center border-t border-b border-gray-400 dark:border-stone-400"
+  >
     count : {{ count }}
   </div>
   <router-view />
 </template>
 
-<script setup  lang="ts">
-import { useCounterStore } from './store'
-
-const { count } = $(useCounterStore())
-</script>
+<style scoped>
+div {
+  a {
+    &.activated {
+      @apply text-teal-600 font-bold;
+    }
+  }
+}
+</style>
