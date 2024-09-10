a44e4727e6eebe9c8556f350912fef6cc61bd87d
toaonly42 <toaonly42@gmail.com>
2024-09-10 14:52:31 +0900

    feat: PostCSS 설정 업데이트
    - 'tailwindcss/nesting' 추가

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,20 +3,21 @@ const url = require('postcss-url')
 
 module.exports = {
   plugins: [
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer'),
-    
+
     url([
       {
         url: 'inline',
         filter: '**/assets/**/*.png',
-        basePath: path.resolve(__dirname, 'src')
+        basePath: path.resolve(__dirname, 'src'),
       },
       {
         url: 'inline',
         filter: '**/assets/**/*.svg',
-        basePath: path.resolve(__dirname, 'src')
-      }
-    ])
+        basePath: path.resolve(__dirname, 'src'),
+      },
+    ]),
   ],
 }
